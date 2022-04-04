0edb68acfc64e5fa2111f8dcfa51e21e9d243fd7
Thanhha2571 <96061135+Thanhha2571@users.noreply.github.com>
2022-04-04 20:58:32 +0700

    Update 1.letranthanhha.ba10-015-1.py

--- 1.letranthanhha.ba10-015-1.py
+++ 1.letranthanhha.ba10-015-1.py
@@ -1,40 +1,62 @@
-def Studentcount():
-    return int(input("Number of students in class are:"));
+def numberofstudents():
+    numberofstudents = int(input("Number of students in class"))
+    return numberofstudents
 
-
-def student_info(numberofstudents):
-    students_list = [];
-    for i in range(numberofstudents):
-        print("Info of student", (i+1), ":");
-        id_s = input("Student ID:");
-        name_s = input("Student name:");
-        dateofbirth = input("Date of birth:");
-        students_list.append ({"- Student ID":id_s, "Student name":name_s, "DoB":dateofbirth})
-    print(students_list);
-
-def Coursecount():
-   return int(input("Number of course:"));
-
-
-def course_info(numberofcourses):
-    courses_list = [];
-    for i in range(numberofcourses):
-        print("Info of course", (i+1), ":");
-        id_c = input("ID course:");
-        name_c = input("Name of course:");
-        courses_list.append ({" - Course ID":id_c, "Name":name_c});
-    print(course_info);
+def StudentInfo(numberofstudents):
+    students = []
+    for i in range(0,numberofstudents):
+        student_id = input("Enter student ID:")
+        student_name = input("Enter student name:")
+        student_dob = input("Enter DoB:") 
+        student ={
+            "Student ID": student_id,
+            "Student name": student_name,
+            "DoB": student_dob,
+            "marks": {}
+        }
+        students.append(student)
+    return students
     
-def findCourse(courses_list,id_c):
-    id_mark = int(intput("Select ID course:"));
-    for course in courses_list:
-        if id_mark == id_c:
-            return name_c;
+def StudentList(students):
+    print("LIST OF ALL STUDENTS")
+    for student in students:
+        print(students)
+        
+def numberofcourses():
+    numberofcourses = int(input("Number of courses"))
+    return numberofcourses
 
-def addMark():
+def CourseInfo(numberofstudents):
+    courses = []
+    for i in range(0,numberofcourses):
+        course_id = input("Enter course ID:")
+        course_name = input("Enter course name:")
+         
+        course ={
+            "Course ID": course_id,
+            "Course name": course_name,
     
-
-numberofstudents = Studentcount();
-students_list = student_info(numberofstudents);
-numberofcourses = Coursecount();
-course_list = course_info(numberofcourses);
+        }
+        courses.append(course)
+    return courses
+def CourseList(courses):
+    print("LIST OF ALL COURSES")
+    for course in courses:
+        print(courses)
+        
+def SelectCourse():
+    CourseList(course)
+    courseid = input("Enter course ID:")
+    return courseid
+    print(f"Enter marks of course {courseid} for students: ")
+    for student in students:
+        mark = float(input(f"- Student {student['student_name']}:"))
+        student ["marks"][courseid] = mark
+numberofstudents = numberofstudents();
+students = StudentInfo(numberofstudents)
+StudentList(students)
+numberofcourses = numberofcourses();
+courses = CourseInfo(numberofstudents)
+CourseList(courses)
+SelectCourse(courses)
+print(students)
